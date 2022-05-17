666c6997a8b9355dd8ecc1f01df41ef15ee6acff
Ramona Gospodinova <ramona.gospodinova@plana.solutions>
2022-05-17 18:42:13 +0300

    SoftUni Tasks .gitignore update

--- demo.py
+++
@@ -1,6 +0,0 @@
-# Dict
-students = {"a": [4, 5, 6], "b": [17, 8, 2, 1]}
-stud = {"Peter": 21, "George": 18, "John": 45, }
-
-# print(sorted(students.items(), key=lambda kvpt: -len(kvpt[1])))
-print(sorted(stud.items(), key=lambda kvpt: kvpt[1], reverse=True))
